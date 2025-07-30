acb7066b9d7c64508852e540a4c8230236cf7fe5
kong <kong504_25@hotmail.co.th>
2025-07-30 14:18:29 +0700

    update: response

---
+++ project/type.ts
@@ -0,0 +1,25 @@
+export interface Project {
+  id: string;
+  year: number;
+  name: string;
+  project_type: string; // category of the project (1: Art and Media, 2: Education, 3: Technology, 4: Research)
+  attribute: string; // ## ใน excel ยังไม่มี eg. Game, Art Toy, Design, etc.
+  chief_id: string; // ผูกกับ id หัวหน้าโครงการ
+  status: number;
+}
+
+export interface Project_detail {
+  id: string;
+  year: number;
+  project_code: string;
+  name: string;
+  project_type: string; // category of the project (1: Art and Media, 2: Education, 3: Technology, 4: Research)
+  attribute: string; // ## ใน excel ยังไม่มี eg. Game, Art Toy, Design, etc.
+  class: string; // ## ใน excel ยังไม่มี (A, B, C, D)
+  chief_id: string; // ผูกกับ id หัวหน้าโครงการ
+  chief_percent: number;
+  sdate: number;
+  edate: number;
+  status: number;
+  create_date: number;
+}
